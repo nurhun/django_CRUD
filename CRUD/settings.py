e1ab15e1f22feeb60b69412fa5f8fc643990c2ad
nurhun <n.a.a.h89@gmail.com>
2021-10-30 04:30:04 +0200

    modify settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'POSTGRES_DB',
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
